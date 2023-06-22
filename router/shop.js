3f1acd1ac2ea03c219e99b24f596965e87729174
Bilal-Tahir <chaudarybilal32@gmail.com>
2023-06-22 18:35:55 +0500

    Remove Pug and Hanlebar

--- router/shop.js
+++ router/shop.js
@@ -7,18 +7,10 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // console.log(adminData.products);
-  // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-
-  // we have defined template engine pug in app.js
-  // we have defined default folder of views where files are present
   res.render('shop', {
     prods: adminData.products,
     pageTitle: 'All Products',
     path: '/',
-    // hasProduct: adminData.products.length > 0, // hbs
-    // productCss: true,  // hbs
-    // activeShop: true, // hbs
   });
 });
 
